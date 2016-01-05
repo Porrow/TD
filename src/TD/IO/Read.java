dbbc2b30f151b6f45cdd9d8e90f38f9ead5da75b
DE DROUAS Marc-Antoine <ETD-P\dedrouas@i12b-20-lx.etd-p.esiea.fr>
2016-01-05 11:42:54 +0100

    premier commit

---
+++ src/TD/IO/Read.java
@@ -0,0 +1,63 @@
+package TD.IO;
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Read
+{
+    public static int[] read(String name)
+    {
+        int data[];
+        try
+        {
+            int n = -1;
+            File f = new File(name);
+            if(f.exists())
+            {
+                System.out.println("taille fichier : "+f.length()+" Byte(s)");
+                data = new int[(int)(f.length()/4)];
+                DataInputStream dis = new DataInputStream(new FileInputStream(f));
+                boolean fin = false;
+                for(int i = 0; !fin; i++)
+                {
+                    try
+                    {
+                        n = dis.readInt();
+                    } 
+                    catch (EOFException ex) {fin = true;}
+                    if(!fin)
+                    {
+                        data[i] = n;
+                        //System.out.println("valeur lue = "+n);
+                    }
+                }
+                dis.close();
+            }
+            else
+            {
+                data = new int[1];
+                data[0] = 0;
+            }
+        }
+        catch(FileNotFoundException fnfe)
+        {
+            data = null;
+            System.out.println("Un fichier n'a pas été trouvé.");
+        }
+        catch (IOException ex) 
+        {
+            data = null;
+            System.out.println("Erreur lors de la lecture d'un fichier.");
+        }
+        catch(NegativeArraySizeException nase)
+        {
+            data = null;
+            System.out.println("Taille de tableau négative.");
+        }
+        return data;
+    }
+}
